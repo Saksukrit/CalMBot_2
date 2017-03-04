7417c0cc4f89554619962cdc08a896abc50b6827
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-03-05 03:30:08 +0700

    .

--- mainbot.php
+++ mainbot.php
@@ -352,11 +352,13 @@ if (!is_null($events['events']))
         $ms_array[1]= $ms_foodlist;
         $ms_array[2]= $ms_foodlist;
 
+        $ms = array($ms_array);
+
         $client->replyMessage(
           array(
             'replyToken' => $event['replyToken'],
             // 'messages' => [$ms_array[1],$ms_array[2]]
-            'messages' => [$ms_array]
+            'messages' => [$ms]
             )
           );
       }
