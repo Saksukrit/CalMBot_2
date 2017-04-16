9521ab45f232c693f8517e60276aa8e79b49a86d
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-04-17 01:28:06 +0700

    .

--- testbot.php
+++ testbot.php
@@ -7,62 +7,68 @@ $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 
 // get replyToken
-$replyToken = $arrJson['events'][0]['replyToken'];
+// $replyToken = $arrJson['events'][0]['replyToken'];
 // url reply
 $strUrl = "https://api.line.me/v2/bot/message/reply";
-
+// datapostback
 $data = array();
-// if (condition) {
-//   # code...
+
+if (!is_null($arrJson['events'])) {
+  foreach ($arrJson['events'] as $event) {
+
+    // get replyToken
+    $replyToken = $event['replyToken'];
+//   }
+//
 // }
-if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($arrJson['events'][0]['message']['text'] == "หลายอัน"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
-}else{
+    if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
+      $data['replyToken'] = $replyToken;
+      $data['messages'][0]['type'] = "text";
+      $data['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
+    }else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+      $data['replyToken'] = $replyToken;
+      $data['messages'][0]['type'] = "text";
+      $data['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+    }else if($arrJson['events'][0]['message']['text'] == "หลายอัน"){
+      $data['replyToken'] = $replyToken;
+      $data['messages'][0]['type'] = "text";
+      $data['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+    }else{
 
-  $messages = [
-  'type' => "text",
-  'text' => "ขอโทษ ฉันไม่เข้าใจ"];
+      $messages = [
+      'type' => "text",
+      'text' => "ขอโทษ ฉันไม่เข้าใจ"];
 
-  $data['replyToken'] = $replyToken;
-  $data['messages'][0] = $messages;
+      $data['replyToken'] = $replyToken;
+      $data['messages'][0] = $messages;
   // $arrPostData['messages'][0]['type'] = "text";
   // $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
-}
+    }
 
 
 
 // Header
-$arrHeader = array();
-$arrHeader[] = "Content-Type: application/json";
-$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+    $arrHeader = array();
+    $arrHeader[] = "Content-Type: application/json";
+    $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
 // json_encode
-$post = json_encode($data);
+    $post = json_encode($data);
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,$strUrl);
-curl_setopt($ch, CURLOPT_HEADER, false);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$result = curl_exec($ch);
-curl_close ($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
 
+  }
+
+}
 // test push by date
 // if (date('Y-m-d') == "2017-04-16") {
 //   $strUrl = "https://api.line.me/v2/bot/message/push";
