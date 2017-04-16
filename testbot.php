33ab28bed1ec81ba8022e8301963b597a3f1ec25
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-04-17 00:04:52 +0700

    .

--- testbot.php
+++ testbot.php
@@ -62,24 +62,24 @@ $result = curl_exec($ch);
 curl_close ($ch);
 
 // test push by date
-if (date('Y-m-d') == "2017-04-16") {
-  $strUrl = "https://api.line.me/v2/bot/message/push";
-  $arrPostData = array();
-  $arrPostData['to'] = "U223a593a6474192e91019c67a657ab7f";
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
-
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL,$strUrl);
-  curl_setopt($ch, CURLOPT_HEADER, false);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  $result = curl_exec($ch);
-  curl_close ($ch);
-}
+// if (date('Y-m-d') == "2017-04-16") {
+//   $strUrl = "https://api.line.me/v2/bot/message/push";
+//   $arrPostData = array();
+//   $arrPostData['to'] = "U223a593a6474192e91019c67a657ab7f";
+//   $arrPostData['messages'][0]['type'] = "text";
+//   $arrPostData['messages'][0]['text'] = "นี้คือการทดสอบ Push Message";
+//
+//   $ch = curl_init();
+//   curl_setopt($ch, CURLOPT_URL,$strUrl);
+//   curl_setopt($ch, CURLOPT_HEADER, false);
+//   curl_setopt($ch, CURLOPT_POST, true);
+//   curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+//   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+//   curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+//   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+//   $result = curl_exec($ch);
+//   curl_close ($ch);
+// }
 
 echo "OK";
 
