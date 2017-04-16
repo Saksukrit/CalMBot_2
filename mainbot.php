63504811e78cc491d51f1c24bbe6d1cf919109b2
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-04-16 18:36:45 +0700

    .

--- mainbot.php
+++ mainbot.php
@@ -959,14 +959,26 @@ if (!is_null($events['events']))
                 $post = json_encode($data);
                 $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-                $ch = curl_init($url);
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                // $ch = curl_init($url);
+                // curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                // curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                // curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                // $result = curl_exec($ch);
+                // curl_close($ch);
+
+
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,$url);
+                curl_setopt($ch, CURLOPT_HEADER, false);
+                curl_setopt($ch, CURLOPT_POST, true);
                 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
                 $result = curl_exec($ch);
-                curl_close($ch);
+                curl_close ($ch);
 
                 echo $result . "\r\n";
             }
