8837a8fb4e49e93cdc2b1129b325d4aa844db009
Saksukrit Ek-uru <kritzastep@gmail.com>
2017-04-17 22:35:23 +0700

    .

---
+++ testword.php
@@ -0,0 +1,48 @@
+<?php
+
+include('THSplitLib/segment.php');
+
+$segment = new Segment();
+$string = "ต้องการบันทึกมื้อมื้ออาหารมื้อ";
+$result = $segment->get_segment_array($string);
+$count = count($result);
+//js_thai_encode($result);
+echo implode(' | ', $result);
+echo '<br>'.$count.'<br><br>';
+
+// $req = explode(" ", "บันทึก มื้อ อาหาร");
+$key = explode(" ", "บันทึก");
+
+if (check_keyword($key,$string) == "true") {
+  echo "บันทึกมื้ออาหาร";
+}else {
+  echo "ไม่ตรงคีย์";
+}
+
+
+
+
+function check_keyword($keyword,$word)
+          {
+            $segment = new Segment();
+            $result = $segment->get_segment_array($word);
+
+            $mapper = 0;
+            // loop mapping algorithm
+            for ($i=0; $i < count($result); $i++) {
+              if ($mapper < count($keyword)) {
+                if ($result[$i] == $keyword[$mapper]) {
+                  $mapper++;
+                }
+              }
+            }
+            // check mapper
+            if ($mapper == count($keyword)) {
+              return "true";
+            }else {
+              return "false";
+            }
+          }
+
+
+ ?>
